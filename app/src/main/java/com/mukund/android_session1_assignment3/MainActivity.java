111bd5a5d35d2059fb240c3c3a96e306807fc82c
MukundPradhan <replyme.mmp@gmail.com>
2016-02-24 17:19:01 +0530

    Initial commit

---
+++ app/src/main/java/com/mukund/android_session1_assignment3/MainActivity.java
@@ -0,0 +1,18 @@
+/*
+This is the Hello World! program to Run in genymotion emulator.
+
+ */
+
+package com.mukund.android_session1_assignment3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
